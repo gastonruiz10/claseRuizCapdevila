310d97b2c3ee75688e081fb3a38d53ba42dca70c
Rodrigo Gastón Ruiz Capdevila <gastonruizcapdevila@gmail.com>
2022-07-21 18:57:09 -0300

    Primer practico JS
    Primer practico JS

---
+++ script.js
@@ -0,0 +1,9 @@
+for (let dia = 1; dia <= 28; dia++) {
+    alert ("Estas en el día "  +dia+  " de tu ciclo");
+    if (dia === 25){
+        alert ("Comenzaste a ovular")
+    }
+    if (dia === 28){
+        alert ("Tu ciclo finalizo")
+    }
+}
